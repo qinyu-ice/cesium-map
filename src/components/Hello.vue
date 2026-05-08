29698dab20e8d05b957e11c9f72f91cf1fd20711
qinyu <2143565179@qq.com>
2026-05-08 15:05:56 +0800

    fix: 修改网站图标和名称

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -8,7 +8,7 @@ const goToMap = () => {
 
 <template>
   <div>
-    <h1>Hello</h1>
+    <h1 style="color: purple;">Welcome to the three-dimensional map!</h1>
     <el-button type="primary" @click="goToMap">跳转地图</el-button>
   </div>
 </template>
